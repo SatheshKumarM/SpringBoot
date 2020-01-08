9fc8cfdc7876173c863a8bc3e5808a15fae65297
Sathesh Kumar <kmrsathesh@gmail.com>
2020-01-08 23:00:42 +0530

    Initial Project creation

---
+++ src/main/java/com/handson/ShoppingCartApplication.java
@@ -0,0 +1,10 @@
+package com.handson;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(ShoppingCartApplication.class,args);
+	}
+	
+}
