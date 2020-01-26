79555e40981d4bf87d81546e488de11f3a24a09c
Sathesh Kumar <kmrsathesh@gmail.com>
2020-01-26 22:20:10 +0530

    Resolved mvn dependencies

--- src/main/java/com/handson/ShoppingCartApplication.java
+++ src/main/java/com/handson/ShoppingCartApplication.java
@@ -1,5 +1,8 @@
 package com.handson;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ShoppingCartApplication {
 
